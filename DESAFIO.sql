8f61fef705cae22852c0a7913bf27ff9b4509dd3
Luisa <92829331+luisastellet@users.noreply.github.com>
2024-09-03 23:07:02 -0300

    Add files via upload

---
+++ DESAFIO.sql
@@ -0,0 +1,52 @@
+-- Questão 1
+SELECT Nome, Ano FROM Filmes
+
+-- Questão 2
+SELECT Nome, Ano, Duracao FROM Filmes
+ORDER BY Ano
+
+-- Questão 3
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Nome = 'De Volta para o Futuro'
+
+-- Questão 4
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Ano = '1997'
+
+-- Questão 5
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Ano > 2000
+
+-- Questão 6
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao
+
+-- Questão 7
+SELECT Ano, COUNT(*) AS Quantidade
+FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC;
+
+-- Questão 8
+SELECT * FROM Atores WHERE Genero = 'M'
+
+-- Questão 9
+SELECT * FROM Atores WHERE Genero = 'F'
+ORDER BY PrimeiroNome
+
+-- Questão 10
+SELECT F.NOME AS NomeFilme, G.Genero AS Genero
+FROM FILMES F
+INNER JOIN FILMESGENERO FG ON F.ID = FG.IDFILME
+INNER JOIN Generos G ON FG.IDGENERO = G.ID;
+
+-- Questão 11
+SELECT F.Nome AS Nome, G.Genero AS Genero
+FROM Filmes F
+INNER JOIN FilmesGenero FG ON F.ID = FG.IdFilme
+INNER JOIN Generos G ON FG.IdGenero = G.Id
+WHERE Genero = 'Mistério';
+
+-- Questão 12
+SELECT F.Nome AS Nome, A.PrimeiroNome AS PrimeiroNome, A.UltimoNome AS UltimoNome, EF.Papel AS Papel
+FROM Filmes F
+INNER JOIN ElencoFilme EF ON F.Id = EF.IdFilme
+INNER JOIN Atores A ON EF.IdAtor = A.Id
+
